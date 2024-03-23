6eab91a63b277c7b3945233792c9a575b4ce8053
jason <cylon.garage@gmail.com>
2024-03-23 19:39:53 +0700

    ble mobile debug7

--- .run.sh
+++ .run.sh
@@ -1,5 +1,5 @@
 rm -r  *
 cp -r ~/workspace/led_controller/app/build/* .
 git add .
-git commit -m 'ble mobile debug5'
-git push origin master
+git commit -m 'ble mobile debug7'
+git push origin main
